43ccdd227de41204d4e39cbe50217a128ffedad2
Sue Qian <xqian0113@gmail.com>
2020-03-02 23:50:22 -0500

    Update ml with tidyverse.R

--- ml with tidyverse.R
+++ ml with tidyverse.R
@@ -1,7 +1,8 @@
 # install.packages('dslabs')
+# install.packages('rsample')
+# install.packages('ranger')
 library(tidyverse)
 library(dslabs)
-library(dplyr)
 
 setwd('C:/Users/angel/Documents/GitHub/ml-with-tidyverse')
 
@@ -176,3 +177,92 @@ fullmodel_perf %>%
   select(country, adj.r.squared)
 
 #### Build, Tune & Evaluate Regression Models ####
+
+library(rsample)
+
+set.seed(42)
+
+# Prepare the initial split object
+gap_split <- initial_split(gapminder, prop = 0.75)
+
+# Extract the training dataframe
+training_data <- training(gap_split)
+
+# Extract the testing dataframe
+testing_data <- testing(gap_split)
+
+# Calculate the dimensions of both training_data and testing_data
+dim(training_data)
+dim(testing_data)
+
+set.seed(42)
+
+# Prepare the dataframe containing the cross validation partitions
+cv_split <- vfold_cv(training_data, v = 5)
+
+cv_data <- cv_split %>% 
+  mutate(
+    # Extract the train dataframe for each split
+    train = map(splits, ~training(.x)), 
+    # Extract the validate dataframe for each split
+    validate = map(splits, ~testing(.x))
+  )
+
+# Use head() to preview cv_data
+head(cv_data)
+
+# Build a model using the train data for each fold of the cross validation
+cv_models_lm <- cv_data %>% 
+  mutate(model = map(train, ~lm(formula = life_expectancy ~ ., data = .x)))
+
+cv_prep_lm <- cv_models_lm %>% 
+  mutate(
+    # Extract the recorded life expectancy for the records in the validate dataframes
+    validate_actual = map(validate, ~.x$life_expectancy),
+    # Predict life expectancy for each validate set using its corresponding model
+    validate_predicted = map2(.x = model, .y = validate, ~predict(.x, .y))
+  )
+
+library(Metrics)
+# Calculate the mean absolute error for each validate fold       
+cv_eval_lm <- cv_prep_lm %>% 
+  mutate(validate_mae = map2_dbl(validate_actual, validate_predicted, ~mae(actual = .x, predicted = .y)))
+
+# Print the validate_mae column
+cv_eval_lm$validate_mae
+
+# Calculate the mean of validate_mae column
+mean(cv_eval_lm$validate_mae)
+
+library(ranger)
+
+# Build a random forest model for each fold
+cv_models_rf <- cv_data %>% 
+  mutate(model = map(train, ~ranger(formula = life_expectancy ~., data = .x,
+                                    num.trees = 100, seed = 42)))
+
+# Generate predictions using the random forest model
+cv_prep_rf <- cv_models_rf %>% 
+  mutate(# Extract the recorded life expectancy for the records in the validate dataframes
+    validate_actual = map(validate, ~.x$life_expectancy),
+    validate_predicted = map2(.x = model, .y = validate, ~predict(.x, .y)$predictions))
+
+# Calculate validate MAE for each fold
+cv_eval_rf <- cv_prep_rf %>% 
+  mutate(validate_mae = map2_dbl(validate_actual, validate_predicted, ~mae(actual = .x, predicted = .y)))
+
+# Print the validate_mae column
+print(cv_eval_rf$validate_mae)
+
+# Calculate the mean of validate_mae column
+mean(cv_eval_rf$validate_mae)
+
+# Prepare for tuning your cross validation folds by varying mtry
+cv_tune <- cv_data %>% 
+  crossing(mtry = 2:5) 
+
+# Build a model for each fold & mtry combination
+cv_model_tunerf <- cv_tune %>% 
+  mutate(model = map2(.x = train, .y = mtry, ~ranger(formula = life_expectancy~., 
+                                                     data = .x, mtry = .y, 
+                                                     num.trees = 100, seed = 42)))
